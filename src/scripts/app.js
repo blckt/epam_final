288f741078273e33ab6b6ab29f687a6cc92ebd2c
Denis Sadilo <Denis Sadilo>
2016-02-18 15:52:45 -0800

    basket.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,20 @@
 function pdpInit() {
-    var btn= document.querySelector('#descr div input[type="button"]');
-console.log(btn);
-btn.onclick=function (e) {
-    btn.classList.add('added');
-    btn.value="PRODUCT ADDED";
+    var btn = document.querySelector('#descr div input[type="button"]');
+    console.log(btn);
+    var sizes= document.querySelector('#descr #sizes');
+    btn.onclick = function (e) {
+        var radiobuttons = document.querySelectorAll('#descr #sizes input');
+        for (var i = 0; i < radiobuttons.length; i++) {
+            if (radiobuttons[i].checked) {
+                btn.classList.add('added');
+                btn.value = "PRODUCT ADDED";
+                return true;
+            }
+        }
+        
+        sizes.classList.toggle('error');
+
+    }
 }
-}
+
+console.log(Basket.itemsCount());
