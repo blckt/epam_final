d8e779a355c0e179001e59c4976b673adde637b2
Denis Sadilo <Denis Sadilo>
2016-02-18 23:43:29 -0800

    basket again

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,20 +1,107 @@
+//LocalStorageWorker
+(function (window) {
+    var basket = {};
+    basket.itemsCount = function () {
+        var items = basket.getItems();
+        if (items) {
+            return Array.from(items).length;
+        } else {
+            return 0;
+        }
+    }
+    basket.getItems = function () {
+        return JSON.parse(localStorage.getItem('basket'));
+    }
+    basket.addItems = function (item) {
+        var items;
+        if (basket.getItems()) {
+            items = Array.from(basket.getItems());
+        }
+        else {
+            items = [];
+        }
+        if (items) {
+            items.push(item);
+            localStorage.setItem('basket', JSON.stringify(items));
+
+        }
+        else {
+            localStorage.setItem('basket', JSON.stringify(item));
+        }
+        localStorageEventHandler();
+    }
+    basket.clearItems = function () {
+        localStorage.removeItem('basket');
+
+        localStorageEventHandler();
+    }
+    basket.removeItem = function (id) {
+        var items = Array.from(basket.getItems());
+        items.splice(id, 1);
+        localStorage.setItem('basket', JSON.stringify(items));
+        localStorageEventHandler();
+    }
+
+    window.Basket = basket;
+
+})(window)
+
+
+
+//Event for storage
+
+window.onstorage = function (e) {
+    localStorageEventHandler();
+}
+function localStorageEventHandler() {
+    document.querySelector('#basket-counter').innerHTML = Basket.itemsCount() || 0;
+
+}
+
+
+//pageWorker
 function pdpInit() {
     var btn = document.querySelector('#descr div input[type="button"]');
-    console.log(btn);
-    var sizes= document.querySelector('#descr #sizes');
+    function pdpDetailsCollector() {
+        var doc = document.querySelector('#item');
+        var item = {};
+        item.img = doc.querySelector('#Main-image').src;
+        item.color = doc.querySelector('#Main-image').alt;
+        doc = doc.querySelector('#descr');
+        item.name = doc.querySelector('div h2').innerHTML;
+        item.article = doc.querySelector('.subheader-small').innerHTML.replace("Article number : ", '');
+        item.price = doc.querySelector('div:nth-child(3)').innerHTML.replace(/\D*/, "");
+        item.size = doc.querySelector('input[type="radio"]:checked').value;
+        Basket.addItems(item);
+
+    }
+    var sizes = document.querySelector('#descr #sizes');
+    var radios = sizes.querySelectorAll('input');
     btn.onclick = function (e) {
-        var radiobuttons = document.querySelectorAll('#descr #sizes input');
-        for (var i = 0; i < radiobuttons.length; i++) {
-            if (radiobuttons[i].checked) {
+
+        for (var i = 0; i < radios.length; i++) {
+            if (radios[i].checked) {
                 btn.classList.add('added');
                 btn.value = "PRODUCT ADDED";
+                pdpDetailsCollector();
                 return true;
             }
         }
-        
+
         sizes.classList.toggle('error');
 
     }
+    for (var i = 0; i < radios.length; i++) {
+        radios[i].onclick = function () {
+            sizes.classList.remove('error');
+            btn.classList.remove('added');
+            btn.value="ADD TO CARD";
+        }
+    }
+
+
 }
 
-console.log(Basket.itemsCount());
+window.onhashchange = function () {
+    getUrlState(decodeURI(window.location.hash, null));
+}
--- src/scripts/router.js
+++ src/scripts/router.js
@@ -2,11 +2,10 @@
 //init page & history api
 (function () {
     getUrlState(null, getRoutes());
+    localStorageEventHandler(); 
 })();
 
-window.onhashchange = function () {
-    getUrlState(decodeURI(window.location.hash, null));
-}
+
 function getRoutes() {
 
     var url = window.location.href.replace(/https?:\/\/\S*:\d*\//, '');
@@ -20,7 +19,7 @@ function getUrlState(hash, routes) {
     if (hash) {
         hash = hash.replace('#', '');
         state = hash.split(/\//)[0];
-        console.log(hash.split(/\//));
+     
         if (hash.split(/\//).length > 1) {
             id = hash.split(/\//)[1];
         }
@@ -61,6 +60,11 @@ function getUrlState(hash, routes) {
 
             break;
         }
+        case 'basket':{
+            initContent('../../Views/basket.html').then(function () {
+                history.pushState({param:'value'},'','../basket');
+            })
+        }
         default: {
             initContent('../../Views/404.html').then(function () {
                 history.pushState({ param: 'value' }, '', '/404');
@@ -70,21 +74,21 @@ function getUrlState(hash, routes) {
     }
 }
 function initProductDetails(id) {
+    
     return new Promise(function (resolve, reject) {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', '../../Data/items.json', true);
+        xhr.open('GET', '../../Data/'+id+'.json', true);
         xhr.send();
         xhr.onreadystatechange = function () {
             if (xhr.readyState != 4) {
                 return;
             }
             else {
-                var items = JSON.parse(xhr.responseText).items;
-                items.forEach(function (element) {
-                    if (element.id == id) {
-                        resolve(element);
-                    }
-                })
+                var items = JSON.parse(xhr.responseText);
+             
+                        resolve(items);
+                    
+               
             }
         }
     });
@@ -95,17 +99,24 @@ function populateProductPage(object) {
     breadcrumbsWay.innerHTML=object.name;
     //main image
     document.querySelector("#Main-image").src=object.img[0];
+    document.querySelector("#Main-image").alt=object.colors[0];
+    
     //images for preview
     for(var i=1;i<object.img.length;i++){
         var img= document.createElement('img');
         img.src=object.img[i];
-        img.alt="Preview Image";
+        img.alt=object.colors[i];
+        
         img.onclick=function (e) {
           
             var main=document.querySelector("#Main-image");
             var temp=e.target.src;
+            var  tempAlt=e.target.alt;
             e.target.src=main.src;
             main.src=temp;
+            e.target.alt=main.alt;
+            main.alt=tempAlt;
+            
             
                     }
         var pr=document.querySelector('.preview');
